4004d3842d2fbcf4055f0863cc066364ba67469f
ii-shimul <islamshimul27@gmail.com>
2025-07-16 03:21:21 +0600

    initial project setup

---
+++ src/main/java/GameFrame.java
@@ -0,0 +1,15 @@
+
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bax
+ */
+public class GameFrame extends JFrame {
+    
+}
---
+++ src/main/java/GamePanel.java
@@ -0,0 +1,21 @@
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bax
+ */
+public class GamePanel extends JPanel implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e){
+        
+    }
+    
+}
