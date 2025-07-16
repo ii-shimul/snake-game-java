bbd608b55041b6b2400ddeb38d1f04af0845b6ec
bax <islamshimul27@gmail.com>
2025-07-16 23:19:34 +0600

    added window setup, made the snake move and apple appear at random places

--- src/main/java/snakegame/GamePanel.java
+++ src/main/java/snakegame/GamePanel.java
@@ -9,7 +9,7 @@ import java.util.Random;
 public class GamePanel extends JPanel implements ActionListener{
     static final int SCREEN_WIDTH = 600;    
     static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 20;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS];
@@ -45,14 +45,51 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public void draw(Graphics g){
+        for(int i = 0; i < (SCREEN_HEIGHT/UNIT_SIZE); i++){
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+        }
         
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        
+        for(int i = 0; i < bodyParts; i++){
+            if(i == 0){
+                g.setColor(Color.green);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else {
+                g.setColor(Color.blue);
+                g.fillOval(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
     
     public void newApple(){
-        
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE)) * UNIT_SIZE;
     }
     
     public void move(){
+        for(int i = bodyParts; i > 0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        
+        switch(direction){
+            case 'U':
+                y[0] = y[0] - UNIT_SIZE;
+                break;
+            case 'D':
+                y[0] = y[0] + UNIT_SIZE;
+                break;
+            case 'L':
+                x[0] = x[0] - UNIT_SIZE;
+                break;
+            case 'R':
+                x[0] = x[0] + UNIT_SIZE;
+                break;
+        }
         
     }
     
@@ -61,6 +98,17 @@ public class GamePanel extends JPanel implements ActionListener{
     }
     
     public void checkCollisions(){
+        // checks head collision with body
+        for(int i = bodyParts; i > 0; i--){
+            if((x[0] == x[i]) && (y[0] == y[i])){
+                running = false;
+            }
+        }
+        
+        // checks head collsion with left border
+        if(x[0] < 0){
+            running = false;
+        }
         
     }
     
@@ -70,7 +118,12 @@ public class GamePanel extends JPanel implements ActionListener{
     
     @Override
     public void actionPerformed(ActionEvent e){
-        
+        if(running){
+            move();
+            checkApple();
+            checkCollisions();
+        }
+        repaint();
     }
     
     public class MyKeyAdapter extends KeyAdapter{
